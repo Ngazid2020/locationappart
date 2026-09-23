3474b25c546ef3ebe52c8f45b71aa080aec8cb14
Ngazid <sn_ngazid@outlook.com>
2026-09-23 18:40:57 +0300

    fix: Increase maximum image upload size to 10320 KB

--- app/Filament/Resources/Properties/Schemas/PropertyForm.php
+++ app/Filament/Resources/Properties/Schemas/PropertyForm.php
@@ -138,7 +138,7 @@ class PropertyForm
                             ->disk('public')
                             ->image()
                             ->imageEditor()
-                            ->maxSize(7320)
+                            ->maxSize(10320)
                             ->required()
                             ->columnSpanFull(),
 
@@ -151,7 +151,7 @@ class PropertyForm
                                     ->disk('public')
                                     ->image()
                                     ->imageEditor()
-                                    ->maxSize(7320)
+                                    ->maxSize(10320)
                                     ->required(),
                             ])
                             ->collapsible()
